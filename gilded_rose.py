08be5ae022f647042ba134f85ee5c546b989d459
cclarke <chris.clarke@heywood.co.uk>
2024-04-03 11:43:50 +0100

    Refactored guilded rose code into separate functions

--- gilded_rose.py
+++ gilded_rose.py
@@ -7,34 +7,50 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+            if item.name == "Sulfuras, Hand of Ragnaros":
+                continue
 
+            if item.name == "Aged Brie":
+                self.update_aged_brie(item)
+            elif item.name == "Backstage passes to a TAFKAL80ETC concert":
+                self.update_tickets(item)
+            else:
+                self.update_normal_item(item)
+
+            item.sell_in -= 1
+
+
+
+    def update_aged_brie(self, item):
+        if item.sell_in >= 0:
+            item.quality += 1
+        else:
+            item.quality += 2
+
+        if item.quality > 50:
+            item.quality = 50
+
+    def update_tickets(self, item):
+        match item:
+            case int if item.sell_in <= 0:
+                item.quality = 0
+            case int if item.sell_in > 10:
+                item.quality += 1
+            case int if item.sell_in > 5:
+                item.quality += 2
+            case int if item.sell_in > 0:
+                item.quality += 3
+        if item.quality > 50:
+            item.quality = 50
+
+    def update_normal_item(self, item):
+        if item.sell_in >= 0:
+            item.quality -= 1
+        else:
+            item.quality -= 2
+
+        if item.quality < 0:
+            item.quality = 0
 
 class Item:
     def __init__(self, name, sell_in, quality):
