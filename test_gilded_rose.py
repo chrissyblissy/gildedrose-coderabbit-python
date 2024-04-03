d9f881addc55d475936a956f2c5f3825f7de8a75
cclarke <chris.clarke@heywood.co.uk>
2024-04-03 10:45:38 +0100

    Added basic unit test to check item name doesn't change when quality updated

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -5,11 +5,21 @@ from gilded_rose import Item, GildedRose
 
 
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
+
+    # helper for running a select number of days of depreciation
+    def run_update_quality_n_times(self, items, days):
         gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertEqual("fixme", items[0].name)
+        for _ in range(days):
+            gilded_rose.update_quality()
+        return items
+
+    # helper for running 1 day of depreciation
+    def run_update_quality_once(self, items):
+        return self.run_update_quality_n_times(items, 1)
+
+    def test_name_does_not_change_when_quality_updated(self):
+        items = self.run_update_quality_once([Item("foo", 0, 0)])
+        self.assertEqual("foo", items[0].name)
 
         
 if __name__ == '__main__':
